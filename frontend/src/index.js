bd418a2044c4a6ded3cf4c4f0f8a5cb9d488d3fc
casa <casa@diogo.local>
2018-07-20 22:31:18 -0300

    refactor: removes now working hoodie code from frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,17 +7,9 @@ import App from './components/App';
 import reducer, { initialState } from './reducers';
 import sagas from './sagas';
 import registerServiceWorker from './registerServiceWorker';
-import PouchDB from 'pouchdb';
-import Hoodie from '@hoodie/client';
-import actions from './actions';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
-
-const hoodie = new Hoodie({
-  url: 'http://localhost:8080',
-  PouchDB: PouchDB
-});
 
 const store = createStore(
   reducer,
@@ -25,7 +17,7 @@ const store = createStore(
   composeEnhancers(applyMiddleware(...middleware))
 );
 
-sagaMiddleware.run(sagas, hoodie);
+sagaMiddleware.run(sagas);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -35,43 +27,3 @@ ReactDOM.render(
 );
 
 registerServiceWorker();
-
-// hoodie event bindings
-
-const d = m => () => store.dispatch(actions.showNotification(m));
-
-hoodie.connectionStatus.on('disconnect', d('disconnected'));
-hoodie.connectionStatus.on('reconnect', d('reconnect'));
-hoodie.connectionStatus.startChecking({ interval: 1000 });
-
-['signin', 'signout', 'passwordreset', 'unauthenticate', 'update'].map(e =>
-  hoodie.account.on(e, d(e))
-);
-
-hoodie.account.on('signup', result =>
-  store.dispatch(actions.authSucessfulAccount(result.username))
-);
-
-hoodie.account.on('signin', result =>
-  store.dispatch(actions.authSucessfulAccount(result.username))
-);
-
-hoodie.account.on('reauthenticate', result =>
-  store.dispatch(actions.authSucessfulAccount(result.username))
-);
-
-// hoodie.store.on('change', () => store.dispatch(actions.pushNotification({content: "Something changed"})));
-// hoodie.store.on('pull', () => store.dispatch(actions.pushNotification({content: "Something was pulled"})));
-//hoodie.store.on('pull', renderItems)
-//hoodie.store.add({name})
-// function renderItems () {
-//   hoodie.store.findAll().then(docs => {
-//     $list.innerHTML = ''
-//     docs.forEach((doc) => {
-//       const $item = document.createElement('li')
-//       $item.textContent = doc.name
-//       $list.appendChild($item)
-//       $input.value = ''
-//     })
-//   })
-// }
--- frontend/src/sagas/index.js
+++ frontend/src/sagas/index.js
@@ -1,30 +1,7 @@
 import { takeLatest } from 'redux-saga/effects';
 
-const signIn = (hoodie, action) => {
-  hoodie.account
-    .signIn(action.payload)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-};
-
-const signOut = hoodie => {
-  hoodie.account
-    .signOut()
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-};
-
-const signUp = (hoodie, action) => {
-  hoodie.account
-    .signUp(action.payload)
-    .then(() => hoodie.account.signIn(action.payload))
-    .catch(err => console.log(err));
-};
-
 function* saga(hoodie) {
-  yield takeLatest('ACCOUNT_SIGNIN', signIn, hoodie);
-  yield takeLatest('ACCOUNT_SIGNOUT', signOut, hoodie);
-  yield takeLatest('ACCOUNT_SIGNUP', signUp, hoodie);
+  yield null;
 }
 
 export default saga;
