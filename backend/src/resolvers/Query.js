4214099e2dca2cd095623408a7a1664e6fac86c7
casa <diogo.alves@gmail.com>
2018-07-23 22:24:53 -0300

    feat: adds a graphql backend

---
+++ backend/src/resolvers/Query.js
@@ -0,0 +1,22 @@
+const { getUserId } = require('../utils');
+
+// todo restrict access to owner
+function notes (parent, args, context, info) {
+  const userId = getUserId(context);
+  const where = {
+    createdBy: {
+      name: userId
+    }
+  }
+  return context.db.query.notes(where, info);
+}
+
+function note (parent, args, context, info) {
+  const userId = getUserId(context);
+  return context.db.query.note({ where: { id: args.id } },info);
+}
+
+module.exports = {
+  notes,
+  note
+} 
